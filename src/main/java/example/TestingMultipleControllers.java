ba04b8da44af827856dfaac4ad9c3a3ca7f859d4
Lawrence Whalen <lawrence.whalen@credera.com>
2022-02-25 16:57:29 -0700

    Create post path for new Person

--- src/main/java/example/TestingMultipleControllers.java
+++ src/main/java/example/TestingMultipleControllers.java
@@ -1,5 +1,6 @@
 package example;
 
+import example.person.Person;
 import example.person.PersonRepository;
 import example.weather.WeatherClient;
 import example.weather.WeatherResponse;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,13 +20,22 @@ public class TestingMultipleControllers {
 
     private final PersonRepository personRepository;
     private final WeatherClient weatherClient;
-    private static final Logger log = LoggerFactory.getLogger(TestingMultipleControllers.class)
+    private final Logger log = LoggerFactory.getLogger(TestingMultipleControllers.class);
 
     @Autowired
-    public ExampleController(final PersonRepository personRepository, final WeatherClient weatherClient) {
+    public TestingMultipleControllers(final PersonRepository personRepository, final WeatherClient weatherClient) {
         this.personRepository = personRepository;
         this.weatherClient = weatherClient;
     }
 
-    @PostMapping("/hello")
+    @PostMapping("/person")
+    public String addPerson(@RequestBody Person newPerson){
+        try {
+            personRepository.save(newPerson);
+        } catch (Exception e) {
+            log.error("Person failed to save.");
+        }
+        log.info("Added " + newPerson.getFirstName() + " " + newPerson.getLastName());
+        return String.format("welcome %s %s!", newPerson.getFirstName(), newPerson.getLastName());
+    }
 }
